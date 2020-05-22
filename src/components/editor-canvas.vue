7bb869e34ad969fae6fa5b398e0ef335626b675a
bojue <lansemaikefeng@126.com>
2020-05-22 15:45:03 +0800

    update

--- src/components/editor-canvas.vue
+++ src/components/editor-canvas.vue
@@ -388,10 +388,10 @@ img {
 .comp-item {
   cursor: move;
   z-index: 1;
+  user-select: none;
 }
 .comp-element img {
   z-index: -1;
-  user-select: none;
 }
 .multipleActive {
   border:1px solid red ;
